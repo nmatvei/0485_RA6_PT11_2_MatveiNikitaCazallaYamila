7f1ff930bf74d02b9e8d4317971661ff08efd3ec
nikita <nikitamatpech@gmail.com>
2025-03-21 10:07:44 +0100

    Versió inicial

---
+++ src/main/java/com/mycompany/_ra6_pt11_2_matveinikitacazallayamila/App.java
@@ -0,0 +1,12 @@
+package com.mycompany._ra6_pt11_2_matveinikitacazallayamila;
+
+/**
+ *
+ * @author nikit
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
