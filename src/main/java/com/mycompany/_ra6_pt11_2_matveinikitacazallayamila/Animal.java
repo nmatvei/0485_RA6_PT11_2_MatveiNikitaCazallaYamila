4593455be8b0f858a7cfb9c57cb0ad11ecb90fe1
ycaza <ycaza@DESKTOP-T1UA6VD>
2025-03-21 11:56:40 +0100

    Primera versió classe Animal

---
+++ src/main/java/com/mycompany/_ra6_pt11_2_matveinikitacazallayamila/Animal.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany._ra6_pt11_2_matveinikitacazallayamila;
+
+/**
+ * Classe Animal
+ * @author Yamila i Nikita
+ */
+public class Animal {
+    
+    private String nom, tipus;
+    private int edat;
+    
+    public Animal() {
+        
+    }
+    
+    public void ferSoroll() {
+        System.out.println("Grrrrrrrr");
+    }
+    
+    public void dormir() {
+        System.out.println("Zzzzzzzz");
+    }
+}
